f0c2ee416efbacf0567228ac0cd7aee2aeb7bb94
Chris M <habibdev96@gmail.com>
2021-02-19 09:38:15 -0700

    created navbar & hero on homepage

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// add background to nav on scroll
+const nav = document.querySelector(".navbar");
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 1) {
+    nav.classList.add("dark");
+  } else {
+    nav.classList.remove("dark");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
