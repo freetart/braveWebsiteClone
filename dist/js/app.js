9d0b7688d146f762d03774f1d668f436728d25a6
Chris M <habibdev96@gmail.com>
2021-02-20 15:57:46 -0700

    created hamburger menu

--- dist/js/app.js
+++ dist/js/app.js
@@ -13,6 +13,29 @@ function fixNav() {
 
 window.addEventListener("scroll", fixNav);
 
+// hamburger menu
+const menuIcon = document.querySelector(".hamburger-menu");
+const menuNavbar = document.querySelector(".menu-navbar");
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
+
+menuIcon.addEventListener("click", () => {
+  menuNavbar.classList.toggle("change");
+  if (menuNavbar.classList.contains("change")) {
+    overlay.classList.remove("hidden");
+    body.style.overflowY = "hidden";
+  } else {
+    overlay.classList.add("hidden");
+    body.style.overflowY = "auto";
+  }
+});
+
+overlay.addEventListener("click", () => {
+  menuNavbar.classList.remove("change");
+  overlay.classList.add("hidden");
+  body.style.overflowY = "auto";
+});
+
 // faq dropdown
 const faqLink = document.querySelectorAll(".faq-content-link");
 
